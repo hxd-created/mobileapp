23227a808c9727d9c961dc77175dc5557433486d
asteriosfm2907 <72600853+asteriosfm2907@users.noreply.github.com>
2020-11-30 14:24:33 +0200

    fixing stilystics and logics

--- src/components/Story/StoryFooter.tsx
+++ src/components/Story/StoryFooter.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components/native';
 import { createFragmentContainer, graphql } from 'react-relay';
 
+
 import VotingBtn from './VotingBtn';
 
 
--- src/components/Story/VotingBtn.tsx
+++ src/components/Story/VotingBtn.tsx
@@ -5,7 +5,6 @@ import { createFragmentContainer, graphql, ReactRelayContext } from 'react-relay
 import ChangeVoteMutation from '../../mutations/Vote/ChangeVoteMutation';
 import HeartIcon from '../../components/Icons/Heart';
 import HeartFilledIcon from '../../components/Icons/HeartFilled';
-import { View } from 'react-native';
 
 
 const VotingBtn = ({vote}) => {
@@ -16,21 +15,22 @@ const VotingBtn = ({vote}) => {
     objectID,
     objectKind,
   } = vote;
-  let Icon;
-  vote.myVote===1 ? Icon=HeartFilledIcon : Icon=HeartIcon;
+  const Icon = vote.myVote === 1 ? HeartFilledIcon : HeartIcon;
 
   const handleLike = ()=>{
     setLoading(true)
-    ChangeVoteMutation(environment, objectKind, objectID, direction, (response, errors) => {
+    console.log(isLoading)
+    isLoading===false && ChangeVoteMutation(environment, objectKind, objectID, direction, (response, errors) => {
       if (errors) {
         console.log(errors.message);
-        setLoading(false)
         return;
       }
+      console.log(isLoading,"2")
+      setLoading(false)
   });
 }
 
-  return (<Container accessibilityState={{disabled:!isLoading}}>
+  return (<Container accessibilityState={{disabled:isLoading}}>
       <LikeBtn onPress={handleLike} activeOpacity={0.5} isLoading={isLoading}>
         <Icon width={25} height={25}/>
         <Text>{vote.votes}</Text>
@@ -55,7 +55,7 @@ export default createFragmentContainer(VotingBtn, {
 const Container = styled.View`
   border-top-color: ${({theme})=>theme.colors.border};
   border-top-width:1px;
-  margin:0px 8px 0px 8px;
+  margin:0px 8px;
 `;
 
 const LikeBtn = styled.TouchableOpacity`
@@ -65,14 +65,17 @@ const LikeBtn = styled.TouchableOpacity`
   justify-content:center;
   align-items:center;
   flex-direction:row;
-  margin:10px 0px 10px 0px;
+  margin:10px -10px;
   ${({isLoading}) => isLoading=false && css`
-    opacity:0.1;
+    opacity:0.5;
   `}
+  position:relative;
 `;
 
 const Text = styled.Text`
   color:white;
   font-size = 25px;
   margin-left:10px;
+  position:absolute;
+  left:35px;
 `;
